2083b37655bcd64ccfaa86f29d09a1400e83db2e
Ilya2006mmm <114501281+Ilya2006mmm@users.noreply.github.com>
2022-10-25 16:11:49 +0000

    uu

---
+++ lab_2_task2.py
@@ -0,0 +1,14 @@
+b = inn(input())
+n = int(input())
+q = int(input())
+
+for i in range(0, n, 1):
+  print(f'interation {i}')
+  
+
+
+
+
+
+
+
--- lab_2_task4.py
+++ lab_2_task4.py
@@ -3,7 +3,18 @@ b = 1
 c = int(input())
 print(a)
 print(b)
-d = a + b
-while d <= c:
-    print(d)
-    b, d = d, b + d
+
+     
+
+#a = 1
+#b = 1
+#c = int(input())
+#print(a)
+#print(b)
+#d = a + b
+#while d <= c:
+#     print(d)
+#     b, d = d, b + d
+
+
+
