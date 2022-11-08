a0f65f3623378d2cdc3a4ad1e887a9b275467345
Ilya2006mmm <114501281+Ilya2006mmm@users.noreply.github.com>
2022-11-08 14:29:28 +0000

    Initial commit

---
+++ lab_3_ task1.py
@@ -0,0 +1,12 @@
+gravity_const = 6.67 * 10 ** (-11) 
+v_sveta = 3 * 10 ** 8
+uskorenie_svobodnogo_padenia = 9.8
+chislo_pi = 3.14
+zarad_elektrona = -1.6 * 10 ** (-19)
+gazovaya_const = 8.31
+
+
+
+
+
+
